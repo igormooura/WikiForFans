33929dfb2425ceca064ec78ae8a0ec98453bffe3
Igor de Oliveira Moura <igor.oliveira.moura@gmail.com>
2025-03-07 15:37:51 -0300

    feat: using express-graphql instead of graphql

--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
-import  Players  from './assets/players.js'; 
+import { ApolloServer, gql } from "apollo-server-express";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import http from "http";
+import express from "express";
+import cors from "cors";
+import Players from './assets/players.js';
 
-const typeDefs = `
+const app = express();
+app.use(cors());
+app.use(express.json());
+const httpServer = http.createServer(app);
+
+const typeDefs = gql`
   enum Position {
     QB
     RB
@@ -59,14 +67,24 @@ const resolvers = {
         player.name.toLowerCase().includes(name.toLowerCase())
       ),
     filterPlayersByTeam: (_, { teamId }) => 
-      Players.filter(player => player.team.id === teamId)
+      Players.filter(player => 
+        player.team.id.toLowerCase() === teamId.toLowerCase()
+      )
   }
 };
-const server = new ApolloServer({ typeDefs, resolvers });
 
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
-});
+const startApolloServer = async (app, httpServer) => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
 
+  await server.start();
+  server.applyMiddleware({ app });
 
-console.log("Server running on " + url);
+  await new Promise(resolve => httpServer.listen({ port: 4000 }, resolve));
+  console.log(`Server ready at http://localhost:4000${server.graphqlPath}`);
+};
+
+startApolloServer(app, httpServer);
